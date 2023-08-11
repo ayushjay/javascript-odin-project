c4bbfc4b8b3d50f5e72db099e223b615b1bf70b9
Ayush J <ayushtime@gmail.com>
2023-08-11 20:47:34 +0530

    added initial files

---
+++ foundations/one.js
@@ -0,0 +1,32 @@
+function add7(num){
+    return  num + 7;
+}
+
+console.log(add7(5));
+
+function multiply(num1, num2){
+    return num1 * num2 ;
+}
+
+
+console.log(multiply(5,7));
+
+function capitalize(str){
+    capStr = str[0].toUpperCase() + str.slice(1);
+  
+    
+
+
+    
+    return capStr;
+}
+
+
+console.log(capitalize("heLLOWorld"));
+
+
+function lastLetter(str){
+    return str.slice(-1);
+}
+
+console.log(lastLetter("HelloWorld"))
